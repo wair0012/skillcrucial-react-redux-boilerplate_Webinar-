0cb8a5600bc08c57ec69b6d885ca806fd7f03b1a
wair0012 <irina_wagner@gmx.de>
2019-11-25 16:51:21 +0100

    Webinar React

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -7,13 +7,68 @@ import { getData } from '../redux/reducers/users'
 
 const Dummy = (props) => {
   const [pageIndex, setPageIndex] = useState(0)
+  // const array = useState(0)  - ist das gleiche wie oben
+  // const pageIndex = array[0]
+  // const setPageIndex =  array[1]
+  const [showText, changeShowText] = useState({
+    toggled1: true,
+    toggled2: false
+  }) // oder useState(true)
+  const toggleSmth = () => {
+    changeShowText({
+      toggled1: !showText.toggled1,
+      toggled2: !showText.toggled2
+    }) // oder changeShowText(!showText)
+  }
   const { getData: getDataProps } = props
   useEffect(() => {
     getDataProps(pageIndex);
   }, [getDataProps, pageIndex])
+  const me = {
+    name: 'Irina',
+    course: 'React',
+    age: 42
+  }
+  const count = 0;
+  if (props.isRequesting) {
+    return 'Requesting...'
+  }
   return (
     <div>
-      <Head title="Hello" />
+      <Head
+        title="Hello" //  <Head  title="Hello"  {...me}  /> soll auch gehen, geht hier aber nicht
+        name={me.name}
+        course={me.course}
+        age={me.age}
+      />
+      {
+        // showText && (<div>Text is shown</div> )
+        // showText.toggled1 && (
+          (count !== 0) && (
+            <div>Text is not shown at all</div>
+          )
+      }
+      <div style={{
+        visibility: !showText.toggled1 ? 'hidden' : '' //   count !== 0 ? {} : { display: 'none' }
+      }}
+      >
+        Text is not shown display none
+      </div>
+      {
+        // showText && (<div>Text is shown</div> )
+        // showText.toggled2
+         false && (
+         <div>Text is shown 2</div>
+         )
+      }
+      {
+         showText.toggled1 ? (
+           <div>Text is toggled</div>
+         ) : (
+           <div>Text is untoggled</div>
+         )
+      }
+      <button type="button" onClick={toggleSmth}>Toggle Me Gently</button>
       <div> {props.isRequesting ? 'Your data is loading' : ''} </div>
       <div> Page {pageIndex} {props.users.length} </div>
       <table>
