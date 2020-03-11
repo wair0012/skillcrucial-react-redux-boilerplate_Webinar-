24cf0243eb28287e28d5313dcd63e8f04b2c1eb1
wair0012 <irina_wagner@gmx.de>
2020-03-11 12:53:45 +0100

    Storage

--- client/components/home.js
+++ client/components/home.js
@@ -5,14 +5,46 @@ import {
 // import { Link } from 'react-router-dom'; // für den Routing in React
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
+import Cookies from 'universal-cookie'
 import Head from './head'
+// import { scaleSequential } from 'd3';
 
 
 const Home = () => {
-  const [counter] = useState(0)
+  const cookies = new Cookies()
+  let initialCounter = 0
+  if (typeof localStorage !== 'undefined'
+    && typeof localStorage.getItem('counter') !== 'undefined') {
+    initialCounter = +localStorage.getItem('counter')
+  }
+  if (typeof sessionStorage !== 'undefined'
+    && typeof sessionStorage.getItem('counter') !== 'undefined') {
+    initialCounter = +sessionStorage.getItem('counter')
+  }
+  if (typeof localStorage !== 'undefined'
+    && typeof localStorage.getItem('counter') !== 'undefined') {
+    initialCounter = +localStorage.getItem('counter')
+  }
+  if (typeof cookies !== 'undefined'
+    && typeof cookies.get('counterCookies') !== 'undefined') {
+    initialCounter = +cookies.get('counterCookies')
+  }
+  const [counter, setCounterNew] = useState(initialCounter)
+  const updateCounter = (counterNew) => {
+    setCounterNew(counterNew)
+    localStorage.setItem('counterlocalStorage', counterNew)
+    sessionStorage.setItem('countersessionStorage', counterNew)
+    cookies.set('counterCookies', counterNew)
+  }
   return (
     <div>
       <Head title="Hello" />
+      <button
+        type="button"
+        onClick={() => updateCounter(counter + 1)}
+      >
+        updateCounter
+      </button>
       <div> Hello World Dashboard{counter} </div>
       <Link to="/salo/pampushi/s/4esnokom">Go for pampushki</Link>
       <br />
